8708ae2d9142207262b560f9588921ca44470933
litong <tong@TONICLI-MB1.local>
2016-06-27 21:08:27 +0800

    fix rebuild-spec

--- test/specs/rebuild-spec.js
+++ test/specs/rebuild-spec.js
@@ -6,7 +6,8 @@ describe('rebuild aid', function () {
     var fullLevelAid = '$1:1$2:2$3:3$4:4$5:5$6:6$7:7$8:8$9:9$10:10$11:11$12:12$13:13$14:14abc$15:wx.user',
         missLevelOnlyAid = '$1:1$2:2$3:3$14:14abc$15:wx.user',
         missValueOnlyAid = '$1:1$2:$3:3$4:4$5:5$6:6$7:7$8:8$9:9$10:10$11:11$12:12$13:13$14:14abc$15:wx.user',
-        missLevelAndValueAid = '$1:1$2:$3:$14:14abc$15:wx.user';
+        missLevelAndValueAid = '$1:1$2:$3:$14:14abc$15:wx.user',
+        nonOrderAid = '$1:6$2:1000$4:7$11:8$5:sdy';
     // rebuilt order 2, 4, 5, 12, 13, 1, 7, 11, 15, 6, 3, 8, 9, 10, 14
     it('rebuild full level', function () {
         expect(rebuild.build(fullLevelAid)).to.equal('$2:2$4:4$5:5$12:12$13:13$1:1$7:7$11:11$15:wx.user$6:6$3:3$8:8$9:9$10:10$14:14abc');
@@ -21,7 +22,7 @@ describe('rebuild aid', function () {
         expect(rebuild.build(missLevelAndValueAid)).to.equal('$2:$1:1$15:wx.user$3:$14:14abc');
     });
     it('rebuild when original aid with non-ordered level', function () {
-        expect(rebuild.build('$1:6$2:1000$4:7$11:8$5:sdy')).to.equal('$2:1000$4:7$5:sdy$1:6$11:8');
+        expect(rebuild.build(nonOrderAid)).to.equal('$2:1000$4:7$5:sdy$1:6$11:8');
     });
 });
 });
